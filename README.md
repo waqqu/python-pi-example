1a61243624b010427ad493fc3e2ab068336e5fb3
waqqu <mapariwaqar326@gmail.com>
2017-08-09 16:01:20 +0530

    newwest commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # python-pi-example
 this is a demo project to demonstrate use of git in ubuntu
+
+making a change
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def main(msg):
-
+  // Add another comment
  // Adding the second comment to this comment
 // Print a message
 
